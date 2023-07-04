decee351ee4932c3f1bf2ec9fe7ffa7d736f2452
MariaCatalinaBurlea <burleamariacatalina@gmail.com>
2023-07-04 22:01:49 +0300

    Implemented the header and begin working on post-edit

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-post-edit',
   templateUrl: './post-edit.component.html',
-  styleUrls: ['./post-edit.component.css']
+  styleUrls: ['./post-edit.component.css'],
 })
-export class PostEditComponent {
+export class PostEditComponent implements OnInit {
+  // ! - null or a value
+  form!: FormGroup;
+  constructor() {}
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null),
+      description: new FormControl(null),
+      imagePath: new FormControl(null),
+      author: new FormControl(null)
+    });
+  }
+
+  onSubmit(){
+    console.log('onSubmit() called!');
+    console.log(this.form);
+  }
 }
